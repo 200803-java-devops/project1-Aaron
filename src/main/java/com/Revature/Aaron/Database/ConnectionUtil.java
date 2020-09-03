140e4be00b8a657cc76f4833d924cb18baf8ec22
AaronDownward <downward150@gmail.com>
2020-09-03 12:06:06 -0500

    quick fix to enable run with mvn exec:java

--- src/main/java/com/Revature/Aaron/Database/ConnectionUtil.java
+++ src/main/java/com/Revature/Aaron/Database/ConnectionUtil.java
@@ -5,13 +5,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionUtil {
-    
+
     public static final String username = "postgres";
     private static final String password = "pass";
     private static final String url = "jdbc:postgresql://localhost:5432/postgres";
 
-
     public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         return DriverManager.getConnection(url, username, password);
     }
 }
